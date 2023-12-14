ac0587c7e861738bb1eb750c4ca9c5b623614a7c
Jamie Hamel‑Smith <jamie3d@gmail.com>
2023-12-13 21:42:14 -0800

    Adds HPD ARX 01c to Proto-GT Challenge

--- src/data/official-sessions.js
+++ src/data/official-sessions.js
@@ -52,7 +52,7 @@ const officials = [
         seriesId: 'gtc',
         shortLabel: 'Proto-GT Challenge',
         label: 'Proto-GT Thrustmaster Challenge',
-        cars: ["Aston Martin DBR9 GT1", "Corvette C6.R GT1", "Ford GT GT2"],
+        cars: ["HPD ARX 01c", "Aston Martin DBR9 GT1", "Corvette C6.R GT1", "Ford GT GT2"],
         links: {
             discord: 'https://discord.gg/jWCPZ8dtnB',
         },
