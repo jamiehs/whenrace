31149ef0343af4141106c18488864f88a8d78209
Jamie Hamel‑Smith <jamie3d@gmail.com>
2024-04-15 07:56:01 -0700

    Change time of Clio Cup SOF due to summer time change.

--- src/data/official-sessions.js
+++ src/data/official-sessions.js
@@ -78,7 +78,7 @@ const officials = [
         sessions: [
             {
                 sessionDay: WED,
-                sessionTimeGmt: '20:00',
+                sessionTimeGmt: '19:00',
                 notes: [SOF]
             },
         ]
